bbdceccaf1cb695398fe16ba862cf4b37752be69
dylon <dmychel@tutanota.com>
2024-06-03 23:55:39 -0400

    jsx edit

--- src/UI/Work.jsx
+++ src/UI/Work.jsx
@@ -1,10 +1,14 @@
 export default function Work() {
   return (
-    <div className="links">
-      <h2>Links to my work</h2>
-      <a href="">Photography</a>
-      <a href="">Web Developer Portfolio</a>
-      <a href="">Crucial Clothing</a>
+    <div className="work">
+      <div className="work_title">
+        <h3>Links to my work</h3>
+      </div>
+      <div className="links">
+        <a href="">Photography</a>
+        <a href="">Web Developer Portfolio</a>
+        <a href="">Crucial Clothing</a>
+      </div>
     </div>
   );
 }

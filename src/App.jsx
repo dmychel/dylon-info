759b1e4c62b04126edbd50695a398824831da6c6
dylon <dmychel@tutanota.com>
2024-06-03 23:38:06 -0400

    import and rendered components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import Contact from "./UI/Contact";
+import Intro from "./UI/Intro";
+import Profile from "./UI/Profile";
+import Work from "./UI/Work";
+
 function App() {
-  return;
+  return (
+    <section className="app">
+      <Profile />
+      <Intro />
+      <Work />
+      <Contact />
+    </section>
+  );
 }
 
 export default App;

bd349dd838c5f5dea431f7c421fe22b55114bb3e
dylon <dmychel@tutanota.com>
2024-06-03 23:11:09 -0400

    created comp

---
+++ src/UI/Profile.jsx
@@ -0,0 +1,11 @@
+export default function Profile() {
+  <div className="profile">
+    <div className="profile_pic">
+      <img src="https://placehold.co/400" alt="" />
+    </div>
+    <div className="name">
+      <h1>Dylon Crowley-Perez</h1>
+      <h3>Creator | Learner</h3>
+    </div>
+  </div>;
+}

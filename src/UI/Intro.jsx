7c1e49bdbdd66140d4aa7f05f601d4b6ccf7fbbe
dylon <dmychel@tutanota.com>
2024-06-03 23:37:53 -0400

    created component

---
+++ src/UI/Intro.jsx
@@ -0,0 +1,12 @@
+export default function Intro() {
+  return (
+    <div className="intro">
+      <p>
+        Ambition and curiosity has leant me many skills and experiences I would
+        have never imagined. From dancing to photography, to web development,
+        and a supervisor role at a major retail corporation, my childlike
+        curiosity has kept my passionate side alive.
+      </p>
+    </div>
+  );
+}
---
+++ src/UI/Work.jsx
@@ -0,0 +1,10 @@
+export default function Work() {
+  return (
+    <div className="links">
+      <h2>Links to my work</h2>
+      <a href="">Photography</a>
+      <a href="">Web Developer Portfolio</a>
+      <a href="">Crucial Clothing</a>
+    </div>
+  );
+}

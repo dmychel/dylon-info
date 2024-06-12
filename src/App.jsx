cb2e5958431c0db4e09c8588173df09b801688ee
dmychel <dmychel@tutanota.com>
2024-06-12 19:56:30 -0400

    change set up to make routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import Contact from "./UI/Contact";
-import Intro from "./UI/Intro";
-import Profile from "./UI/Profile";
-import Work from "./UI/Work";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "/src/routes/Index";
+import CrucialClothing from "/src/routes/CrucialClothing";
+import Photography from "/src/routes/Photography";
+import Portfolio from "/src/routes/Portfolio";
+import Error from "./routes/Error";
 
 function App() {
   return (
-    <section className="app">
-      <Profile />
-      <Intro />
-      <Work />
-      <Contact />
-    </section>
+    <BrowserRouter>
+      <section className="app">
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/crucialclothing" element={<CrucialClothing />} />
+          <Route path="/photography" element={<Photography />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </section>
+    </BrowserRouter>
   );
 }
 

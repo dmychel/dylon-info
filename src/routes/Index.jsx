b0df5cfc88edce8db500f3b351520aed029a9b8b
dmychel <dmychel@tutanota.com>
2024-06-12 19:57:07 -0400

    created component

---
+++ src/routes/Index.jsx
@@ -0,0 +1,15 @@
+import Contact from "/src/components/UI/Contact";
+import Intro from "/src/components/UI/Intro";
+import Profile from "/src/components/UI/Profile";
+import Work from "/src/components/UI/Work";
+
+export default function Index() {
+  return (
+    <>
+      <Profile />
+      <Intro />
+      <Work />
+      <Contact />
+    </>
+  );
+}

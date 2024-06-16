25288baa4c18af887850767f87dd16afa85b277a
dmychel <dmychel@tutanota.com>
2024-06-16 16:40:10 -0400

    created component

---
+++ src/components/UI/work/Work.jsx
@@ -0,0 +1,13 @@
+import CrucialClothing from "/src/components/UI/work/CrucialClothing";
+import Photography from "/src/components/UI/work/Photography";
+import Portfolio from "/src/components/UI/work/Portfolio";
+
+export default function Work() {
+  return (
+    <div className="work">
+      <CrucialClothing />
+      <Photography />
+      <Portfolio />
+    </div>
+  );
+}

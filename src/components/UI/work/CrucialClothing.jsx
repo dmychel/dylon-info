a063e542705820ba27ccf2c298f1cb0eda0bbf49
dmychel <dmychel@tutanota.com>
2024-06-16 16:39:52 -0400

    created jsx

---
+++ src/components/UI/work/CrucialClothing.jsx
@@ -0,0 +1,39 @@
+export default function CrucialClothing() {
+  return (
+    <div className="crucial_clothing main_element">
+      <div className="title">
+        <div className="hero">
+          <img src="/assets/imgs/crucial.png" alt="" />
+        </div>
+        <h2>Crucial Clothing</h2>
+      </div>
+
+      <div className="project_info">
+        <p>
+          Crucial Clothing started off as a passion project built to adress an
+          issue in the fashion industry. We believe consumer products served to
+          the community should be driven by the community. When entering{" "}
+          <a href="becrucial.club">becrucial.club</a> you will notice a section
+          to leave feedback and clothing items/styles you would like to see in
+          the future from Crucial Clothing. Community and Culture drives Crucial
+          Clothing.
+        </p>
+
+        <ul>
+          <li>
+            <img src="/assets/icons/world-wide-web.png" alt="" />{" "}
+            <a href="becrucial.club">Website</a>
+          </li>
+          <li>
+            <img src="/assets/icons/instagram.png" alt="" />{" "}
+            <a href="becrucial.club">Instagram</a>
+          </li>
+          <li>
+            <img src="/assets/icons/mail.png" alt="" />{" "}
+            <a href="becrucial.club">Email</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}

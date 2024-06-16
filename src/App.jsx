b0e98717dfac51e44f92fc1c776123404472b379
dmychel <dmychel@tutanota.com>
2024-06-16 16:39:34 -0400

    removed routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "/src/routes/Index";
-import CrucialClothing from "/src/routes/CrucialClothing";
-import Photography from "/src/routes/Photography";
-import Portfolio from "/src/routes/Portfolio";
 import Error from "./routes/Error";
 
 function App() {
   return (
     <BrowserRouter>
       <section className="app">
+        <header>
+          <h1>Dylon Crowley-Perez</h1>
+        </header>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/crucialclothing" element={<CrucialClothing />} />
-          <Route path="/photography" element={<Photography />} />
-          <Route path="/portfolio" element={<Portfolio />} />
           <Route path="*" element={<Error />} />
         </Routes>
       </section>

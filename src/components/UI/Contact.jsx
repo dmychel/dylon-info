2e00f370eaf4a01b637e419036e1943c59e9ae16
dmychel <dmychel@tutanota.com>
2024-07-06 19:03:21 -0400

    import css and jsx edit

--- src/components/UI/Contact.jsx
+++ src/components/UI/Contact.jsx
@@ -1,7 +1,9 @@
+import "/src/App.scss";
+
 export default function Contact() {
   return (
-    <div className="contact">
-      <h3>Contact</h3>
+    <div className="contact main_element">
+      <h2>Contact</h2>
       <p>
         If you are looking to email directly, you can do so at{" "}
         <a href="mailto:dyloncrowley86@gmail.com">DylonCrowley86@gmail.com</a>.

2f8c77c68ca6adf91b26d82ac5deee7f9402a21d
dylon <dmychel@tutanota.com>
2024-07-05 23:44:46 -0400

    created component

---
+++ src/UI/Contact.jsx
@@ -0,0 +1,11 @@
+export default function Contact() {
+  return (
+    <div className="contact">
+      <h3>Contact</h3>
+      <p>
+        If you are looking to email directly, you can do so at{" "}
+        <a href="mailto:dyloncrowley86@gmail.com">DylonCrowley86@gmail.com</a>.
+      </p>
+    </div>
+  );
+}

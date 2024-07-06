6882a79c871c858a36a780a44ff4dde065d79af2
dylon <dmychel@tutanota.com>
2024-07-05 23:44:36 -0400

    created component

---
+++ src/components/UI/work/Developer.jsx
@@ -0,0 +1,9 @@
+export default function Developer() {
+  return (
+    <div className="developer main_element">
+      <div className="title">
+        <h2>Web Development</h2>
+      </div>
+    </div>
+  );
+}

d470ca8970285f284e719d385dff5f1dcf9af59a
dylon <dmychel@tutanota.com>
2024-07-06 00:09:20 -0400

    removed import

--- src/components/UI/work/Work.jsx
+++ src/components/UI/work/Work.jsx
@@ -1,14 +1,12 @@
 import Developer from "./Developer";
 import CrucialClothing from "/src/components/UI/work/CrucialClothing";
 import Photography from "/src/components/UI/work/Photography";
-import Portfolio from "/src/components/UI/work/Portfolio";
 
 export default function Work() {
   return (
     <div className="work">
       <CrucialClothing />
       <Photography />
-      <Portfolio />
       <Developer />
     </div>
   );

89bad61cd59fc43a4af52a4d41f8a8ead6c75395
dylon <dmychel@tutanota.com>
2024-07-06 00:11:14 -0400

    added jsx

--- src/components/UI/work/Developer.jsx
+++ src/components/UI/work/Developer.jsx
@@ -1,8 +1,28 @@
 export default function Developer() {
   return (
     <div className="developer main_element">
+      <div className="hero">
+        <img src="/assets/imgs/portfolio.png" alt="" />
+      </div>
       <div className="title">
         <h2>Web Development</h2>
+      </div>
+      <div className="project_info">
+        <p>
+          I began my Web Development back in 2022. These past couple of years
+          have been a wild ride, not exactly smooth sailing, but I wouldn&apos;t
+          change a thing. Teaching myself to code has been a rollercoaster of
+          highs and lows, but it&apos;s given me the confidence to tackle and
+          learn anything I need to. There were moments when I felt totally
+          demoralized, but look at this website right now—I built it from
+          scratch. You get internal value form creating something from nothing.
+        </p>
+        <div className="link">
+          <p>Take a look at my</p>
+          <a href="https://dyloncrowley.dev" target="_blank">
+            PORTFOLIO
+          </a>
+        </div>
       </div>
     </div>
   );

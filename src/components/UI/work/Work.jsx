b2aa5e1e5bb602e9f2106310c21a66c3d59a52c7
dylon <dmychel@tutanota.com>
2024-07-05 23:44:12 -0400

    import and rendered developer component

--- src/components/UI/work/Work.jsx
+++ src/components/UI/work/Work.jsx
@@ -1,3 +1,4 @@
+import Developer from "./Developer";
 import CrucialClothing from "/src/components/UI/work/CrucialClothing";
 import Photography from "/src/components/UI/work/Photography";
 import Portfolio from "/src/components/UI/work/Portfolio";
@@ -8,6 +9,7 @@ export default function Work() {
       <CrucialClothing />
       <Photography />
       <Portfolio />
+      <Developer />
     </div>
   );
 }
